b31d99a83f3cc6a1a8d069773ad69951a2e4c741
Edi Siswanto <edisiswanto.ti8.poliwangi@gmail.com>
2020-08-05 16:11:51 +0700

    add isotop layout

---
+++ js/custom.js
@@ -0,0 +1,24 @@
+!(function ($) {
+  "use strict";
+
+  $(window).on("load", function () {
+    var portfolioIsotope = $(".portfolio-container").isotope({
+      itemSelector: ".portfolio-item",
+    });
+
+    $("#portfolio-flters li").on("click", function () {
+      $("#portfolio-flters li").removeClass("filter-active");
+      $(this).addClass("filter-active");
+
+      portfolioIsotope.isotope({
+        filter: $(this).data("filter"),
+      });
+      // aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    // $(document).ready(function () {
+    //   $(".venobox").venobox();
+    // });
+  });
+})(jQuery);
